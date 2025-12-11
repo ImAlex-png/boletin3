69ebe1a228f18d24f92dbafeadeca1b3f98061a4
alex1608-cell <alejc2006@gmail.com>
2025-12-11 12:11:42 +0100

    terminado act sin simulacro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Alumno } from './models/alumno';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
